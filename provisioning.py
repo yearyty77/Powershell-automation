52106d10e28695a8d83b7d6d19fb36217deee80d
yearyty77 <tyler.yeary_ic@wwwinc.com>
2026-04-08 17:13:49 -0400

    cleaning up code and adding .env file for credentials

--- provisioning.py
+++ provisioning.py
@@ -1,3 +1,6 @@
+from access_logic import generate_username
+
+
 def provision_user_access(request):
     """
     Substitute API calls in later...
@@ -16,12 +19,13 @@ def provision_user_access(request):
         handle_existing_user(user, requested_roles)
     else:
         handle_new_user(user, base_username, requested_roles)
-
-        def check_user_exists_in_centric(username):
-    """
-    Substitute Centric API call in later.
-    """
-    raise NotImplementedError("Centric API not integrated yet")
+    
+    
+    def check_user_exists_in_centric(username):
+        """
+        Substitute Centric API call in later.
+        """
+        raise NotImplementedError("Centric API not integrated yet")
 
 
 def handle_new_user(user, username, roles):
@@ -36,4 +40,4 @@ def handle_existing_user(user, roles):
     Substitute API call later for updating an existing user.
     """
     print(f"[PLAN] Update user {user.get('email')} with roles {roles}")
-``
+repr
